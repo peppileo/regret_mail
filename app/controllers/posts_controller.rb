595302643c8464e77e25d16f2f2924bdc6d2d4ed
peppileo <ryu.0321.leo@icloud.com>
2023-09-30 17:34:18 +0900

    投稿新しい順

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,20 @@ class PostsController < ApplicationController
   def new
   end
 
-  def index
-    @posts = Post.all.order(created_at: :desc)
-  end
-
-
   # def index
-  #   if params[:latest]
-  #     @posts = Post.latest
-  #   elsif params[:old]
-  #     @posts = Post.old
-  #   else
-  #     @posts = Post.latest
-  #   end
+  #   @posts = Post.all.order(created_at: :desc)
   # end
+
+
+  def index
+    if params[:latest]
+      @posts = Post.latest
+    elsif params[:old]
+      @posts = Post.old
+    else
+      @posts = Post.latest
+    end
+  end
 
   def create
     @post = Post.new(name: params[:name], sub: params[:sub],content: params[:content], datetime: params[:datetime])
